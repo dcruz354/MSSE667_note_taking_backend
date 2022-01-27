935922c4543eb5f5dae92c13608007d5489f23f9
dcruz354 <dcruz354@gmail.com>
2022-01-26 21:50:29 -0800

    first commit

---
+++ src/main/java/com/notetakingbackend/Msse667NoteTakingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notetakingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msse667NoteTakingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msse667NoteTakingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notetakingbackend/Msse667NoteTakingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notetakingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msse667NoteTakingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
